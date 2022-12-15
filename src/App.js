f0b792c84066c1a30eea60df97429a0280f1999f
John Doe <leehy0782@naver.com>
2022-12-15 18:06:53 +0900

    배포 테스트

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const Error = lazy(() => import("./pages/Error"));
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="https://minbok-1998.github.io/wanted-pre-onboarding-frontend/">
+      <BrowserRouter>
         <Suspense>
           <Routes>
             <Route path="/" element={<Login />} />
